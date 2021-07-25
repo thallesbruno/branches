66534cf44ddb91ae112012b86cbb148fabbc2a6f
Thalles Bruno <thallesbgns@gmail.com>
2021-07-25 10:31:28 -0300

    Commit dia posterior

--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ namespace branches
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int z = 10;
-            Console.WriteLine("Número informado: " + z);
-            int y = 10;
-            Console.WriteLine("Segundo número informado: " + y);
+            int w = 10;
+            Console.WriteLine("Primeiro úmero informado: " + w);
+            int x = 11;
+            Console.WriteLine("Segundo número informado: " + x);
+            int y = 12;
+            Console.WriteLine("Terceiro número informado: " + y);
         }
     }
 }
