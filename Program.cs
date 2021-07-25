c05f31c224566d5211cc6e9e97d0d9afe140b1b5
Thalles Bruno <thallesbgns@gmail.com>
2021-07-24 23:16:00 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace branches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int z;
+        }
+    }
+}
